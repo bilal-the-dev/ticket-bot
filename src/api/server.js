67be5c4ec949d85f174ff3618d7ed1bbd7438406
bilal-the-dev <bilalfarooq6001@gmail.com>
2024-10-18 10:15:36 +0500

    s

--- src/api/server.js
+++ src/api/server.js
@@ -1,10 +1,46 @@
-const http = require("http");
+const http = require("node:http");
 
 const discordClient = require("../bot");
+const client = require("../bot");
 
+const { BASE_URL, PORT = 8080 } = process.env;
 http
   .createServer(function (req, res) {
-    res.write(discordClient); //write a response to the client
-    res.end(); //end the response
+    try {
+      const { url, method } = req;
+
+      if (!url.startsWith(BASE_URL) || !client.isReady()) return res.write();
+
+      console.log(req.headers);
+      console.log(req.trailers);
+
+      req.on("error", console.log);
+      res.on("error", console.log);
+
+      if (method === "POST") {
+        let body = [];
+        req
+          .on("data", (chunk) => {
+            // console.log(chunk);
+
+            body.push(chunk);
+          })
+          .on("end", () => {
+            body = Buffer.concat(body).toString();
+            console.log(body);
+
+            const guilds = JSON.parse(body);
+
+            const filteredGuilds = guilds.filter((g) =>
+              client.guilds.cache.has(g.id)
+            );
+            res.end(JSON.stringify(filteredGuilds));
+          });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   })
-  .listen(8080);
+  .listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
